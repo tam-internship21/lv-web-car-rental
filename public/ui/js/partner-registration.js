665fc7545bcb26381fe616c803379b37b8c60655
TRANNGOCTAM <tran.ngoc.tam@uruca.jp>
2024-03-27 11:48:18 +0700

    perf: update code and set up docker.

--- public/ui/js/partner-registration.js
+++
@@ -1,9 +0,0 @@
-//Show button partner registration
-const showPartner = document.querySelectorAll('.regis-partner-car .show-partner')
-const btnRegis = document.querySelectorAll('.regis-partner-car .block-full.btn-regis')
-showPartner.forEach((e) => {
-    e.addEventListener('click', () => {
-        e.classList.toggle('active')
-    })
-
-})
